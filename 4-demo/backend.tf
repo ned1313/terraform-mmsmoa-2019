bb8fc0d8f9aa7c8299d9e8d4e93b36b0efcf6d19
ned1313 <ned.bellavance@gmail.com>
2019-04-25 18:27:42 -0400

    add resource group back

--- 4-demo/backend.tf
+++ 4-demo/backend.tf
@@ -3,5 +3,6 @@ terraform {
     storage_account_name = "mms2019remotestate"
     container_name       = "mms2019-remotestate-demos"
     key                  = "4-demo.state"
+    resource_group_name = "mms2019-setup"
   }
 }
