7a58caf8c191c3e7ae0eba7f942d4de35ccf4e77
ned1313 <ned.bellavance@gmail.com>
2019-04-25 15:07:54 -0400

    add MSI env variable

--- 4-demo/backend.tf
+++ 4-demo/backend.tf
@@ -3,6 +3,5 @@ terraform {
     storage_account_name = "mms2019remotestate"
     container_name       = "mms2019-remotestate-demos"
     key                  = "4-demo.state"
-    use_msi              = true
   }
 }
